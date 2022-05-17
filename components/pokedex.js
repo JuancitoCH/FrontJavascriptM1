8c2509134c19905ec468161cd1f31f529c6f1b90
Juancito <cjuan.chona@hotmail.com>
2022-05-17 18:15:14 -0300

    Pokedex con sprite pokemons

--- components/pokedex.js
+++ components/pokedex.js
@@ -1,41 +1,67 @@
 import { pokeNames } from '../components/pokename.js'
 
-
 const pokedex = async () => {
     const { results } = await fetch('https://pokeapi.co/api/v2/pokemon/', {
         method: 'GET',
         // mode:"no-cors"
     }).then(data => data.json())
 
-
     const SectionPokedex = document.createElement('section')
     const pokedex = document.createElement('article')
+    const vistaPokemon = document.createElement('article')
     const listPokemons = document.createElement('article')
+    // const listPokemons = document.createElement('article')
 
     SectionPokedex.classList.add('main_pokedex')
     pokedex.classList.add('pokedex_layout')
+    vistaPokemon.classList.add('view_pokemon')
     listPokemons.classList.add('pokedex_list_pokemons')
-    
+
     results.forEach(dataP => {
         pokeNames(listPokemons, dataP)
     });
-    pokedex.ondragover =e=>e.preventDefault()
-    pokedex.ondrop = (e)=>{
-        e.preventDefault()
-        const pokemondata =e.dataTransfer.getData('url')
-        // console.log(pokemondata)
-        const div = document.getElementById(pokemondata)
-        console.log(div)
-        pokedex.appendChild(div)
-        console.log(div.getAttribute('url'))
-
-    }
-
-
+    pokedex.ondragover = e => e.preventDefault()
+    pokedex.ondrop = async(e)=>await pokemonOndrop(e,pokedex,vistaPokemon)
+    
+    pokedex.appendChild(vistaPokemon)
     SectionPokedex.appendChild(listPokemons)
     SectionPokedex.appendChild(pokedex)
     return SectionPokedex
 }
 
-export {pokedex}
+
+const pokemonRequest = async (urlP) => {
+    const results = await fetch(urlP, {
+        method: 'GET',
+        // mode:"no-cors"
+    }).then(data => data.json())
+    console.log(results)
+    return results
+}
+
+const pokemonOndrop=async(e,pokedex,vistaPokemon) => {
+    e.preventDefault()
+    const pokemondata = e.dataTransfer.getData('url')
+    const div = document.getElementById(pokemondata)
+
+    pokedex.appendChild(div)
+
+    const urlP = div.getAttribute('url')
+    const pokemon = await pokemonRequest(urlP)
+
+    const elemntForView=await dexPokemon(pokemon)
+    vistaPokemon.innerHTML =''
+    vistaPokemon.appendChild(elemntForView)
+
+}
+
+const dexPokemon =async(data)=>{
+    const sprite = document.createElement('img')
+    sprite.setAttribute('src',data.sprites.front_default)
+    return sprite
+}
+
+
+
+export { pokedex }
 
